17c767e15bd8c112ffef45f9e54e3f546937e72a
Kavindu Tharaka <126257000+Kavindutharaka@users.noreply.github.com>
2024-07-29 12:06:12 +0530

    comment Web Config

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,39 +1,39 @@
 package com.example.demo;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.core.io.Resource;
+// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.web.servlet.resource.PathResourceResolver;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+// import java.io.IOException;
+// import java.nio.file.Path;
+// import java.nio.file.Paths;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/site/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected org.springframework.core.io.Resource getResource(String resourcePath, org.springframework.core.io.Resource location) throws IOException {
-                        org.springframework.core.io.Resource requestedResource = location.createRelative(resourcePath);
-                        System.out.println(requestedResource);
-                        if (requestedResource.exists() && requestedResource.isReadable()) {
-                            System.out.println("f");
-                            return requestedResource;
-                        }
-                        Resource indexResource = location.createRelative(resourcePath + "/index.html");
-                        if (indexResource.exists() && indexResource.isReadable()) {
-                            return indexResource;
-                        }
-                        return null;
-                    }
-                });
-    }
-}
+//     @Override
+//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//         registry.addResourceHandler("/**")
+//                 .addResourceLocations("classpath:/site/")
+//                 .resourceChain(true)
+//                 .addResolver(new PathResourceResolver() {
+//                     @Override
+//                     protected org.springframework.core.io.Resource getResource(String resourcePath, org.springframework.core.io.Resource location) throws IOException {
+//                         org.springframework.core.io.Resource requestedResource = location.createRelative(resourcePath);
+//                         System.out.println(requestedResource);
+//                         if (requestedResource.exists() && requestedResource.isReadable()) {
+//                             System.out.println("f");
+//                             return requestedResource;
+//                         }
+//                         Resource indexResource = location.createRelative(resourcePath + "/index.html");
+//                         if (indexResource.exists() && indexResource.isReadable()) {
+//                             return indexResource;
+//                         }
+//                         return null;
+//                     }
+//                 });
+//     }
+// }
